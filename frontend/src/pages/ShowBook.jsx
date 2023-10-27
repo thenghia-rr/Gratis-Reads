a976246d95c0052ef5271d191dbe5688d67ab17b
thenghia <nghiadang085@gmail.com>
2023-10-27 21:19:29 +0700

    change

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -13,7 +13,7 @@ const ShowBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://localhost:5555/books/${id}`)
+      .get(`https://gratis-reads-server.onrender.com/books/${id}`)
       .then((res) => {
         setBook(res.data.data);
         setLoading(false);
