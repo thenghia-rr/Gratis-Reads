86e8be014842371394272c9ab518238153192af5
thenghia <nghiadang085@gmail.com>
2023-10-27 21:07:45 +0700

    fixed navbar

--- frontend/src/components/header/NavBar.jsx
+++ frontend/src/components/header/NavBar.jsx
@@ -16,20 +16,20 @@ const NavBar = () => {
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <Link to="/" className="flex items-center">
               <SiBookstack className="mr-3 h-6 sm:h-9 text-sky-500 text-3xl" />
-              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white text-sky-500">
+              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-sky-500 text-sky-500 max-sm:text-[17px]">
                 GratisReads
               </span>
             </Link>
             <div className="flex items-center lg:order-2">
               <Link
                 to="/login"
-                className="text-gray-800 hover:text-sky-500 dark:text-white dark:hover:text-sky-500  transition-colors hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+                className="max-sm:text-[13px] text-gray-800 hover:text-sky-500 dark:text-white dark:hover:text-sky-500  transition-colors hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
               >
-                Log in
+                Log In
               </Link>
               <Link
                 to="/signup"
-                className="dark:text-white hover:text-sky-500 dark:hover:text-sky-500 transition-colors focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+                className="max-sm:text-[13px] dark:text-white hover:text-sky-500 dark:hover:text-sky-500 transition-colors focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
               >
                 Sign Up
               </Link>
